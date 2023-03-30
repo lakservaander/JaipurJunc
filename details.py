737206a7d633acbdb31eb8af57db995e4644a365
Vawenker <128976233+Vawenker@users.noreply.github.com>
2023-03-30 18:46:26 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
 api_id = "21609776"
 api_hash = "61f2558d634df353a2a9806661f4b091"
-bot_token = "6105930158:AAGYGLrp7ltqJshcrkAOI9htIiH6SL-Kc70"
+bot_token = "5990866161:AAH4JgMUHv7Nx5Nw2nTe4r2CxU1tr5Ay10U"
 sudo_groups = -1001542586507
